547147f333be298969182f2a9195d6287fd18712
dungngu1345 <ldng96162@gmail.com>
2024-03-18 23:42:28 +0700

    làm tiếp điểm

--- ASM2024_FAP/src/java/controller/lecturer/ScoreController.java
+++ ASM2024_FAP/src/java/controller/lecturer/ScoreController.java
@@ -6,13 +6,16 @@
 package controller.lecturer;
 
 import controller.authentication.BaseRequiredAuthenticationController;
+import dal.GradeDBContext;
+import dal.LessionDBContext;
 import entity.Account;
+import entity.Grade;
+import entity.Lession;
 import java.io.IOException;
-import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
 
 /**
  *
@@ -26,7 +29,16 @@ public class ScoreController extends BaseRequiredAuthenticationController {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp, Account account) throws ServletException, IOException {
+        String studentidString = req.getParameter("sid");
+        int studentid = Integer.parseInt(studentidString);
+        int subjectid = 0;
+        if (req.getParameter("subid") != null) {
+            subjectid = Integer.parseInt(req.getParameter("subid"));
+        }
+        GradeDBContext gra = new GradeDBContext();
+        ArrayList<Grade> gras = gra.getGradebyStudent(studentid);
+        req.setAttribute("score", gras);
         req.getRequestDispatcher("View/Score/ViewScoreStudent.jsp").forward(req, resp);
     }
 
-}
+}   
